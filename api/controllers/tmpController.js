6e88839d9fd130ba3f09dae9b9bc35056795720d
KCM <kevinmenezes@yahoo.com>
2017-07-12 23:31:09 -0700

    added logger

--- api/controllers/tmpController.js
+++ api/controllers/tmpController.js
@@ -3,10 +3,18 @@
  */
 'use strict';
 
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+logger.level = 'debug';
+
 exports.get_readings = function(req, res) {
     res.send('reached GET')
 };
 
 exports.post_readings = function(req, res) {
+
+    logger.debug('entityId: ' + req.query.entityId);
+    logger.debug('celsius reading: ', req.query.celsius);
+
     res.send('reached POST')
 };
